06f88e6f29ec2b24c8774245b3d88d84b33295b9
handsomeOne <i@handsomeone.com>
2016-11-03 11:00:28 +0800

    proper loader order

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     loaders: [
       {
         test: /\.scss$/,
-        loaders: ['style', 'css', 'sass', 'postcss'],
+        loaders: ['style', 'css', 'postcss', 'sass'],
       },
       {
         test: /\.js$/,
